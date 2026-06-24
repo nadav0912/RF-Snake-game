0e74649f152a52d4ebd4e3cde1cd4bdb47fab7ec
Nadav Izhaki <nadav.izh@gmail.com>
2026-06-24 09:46:08 +0300

    delete reward on getting closer tot the apple ans set negative reward (punish) on every step no matter the direction

--- game.py
+++ game.py
@@ -83,23 +83,26 @@ class SnakeGameAi:
                 quit()
 
         # Dist from the apple before move
-        dist_before = math.sqrt((self.head.x - self.food.x)**2 + (self.head.y - self.food.y)**2)
+        #dist_before = math.sqrt((self.head.x - self.food.x)**2 + (self.head.y - self.food.y)**2)
         
         # 2. move
         self._move(action) # update the head
         self.snake.insert(0, self.head)
 
         # Dist from the apple after move
-        dist_after = math.sqrt((self.head.x - self.food.x)**2 + (self.head.y - self.food.y)**2)
+        #dist_after = math.sqrt((self.head.x - self.food.x)**2 + (self.head.y - self.food.y)**2)
         
+        """
         # 3. Small reward on move away/move closer to the apple
         reward = 0
         if dist_after < dist_before:
             reward = 0.3  
         else:
             reward = -0.3 
+        """
 
         # 4. check if game over
+        reward = -0.1
         game_over = False
         if self.is_collision() or self.frame_iteration > 100*len(self.snake):
             game_over = True
